4515bd73736cf70b5b61763ee9226b86bcdb3cbd
duong91002 <96910467+duong91002@users.noreply.github.com>
2023-05-18 19:27:37 +0700

    done statistic

--- src/main/resources/static/assets/js/index.js
+++ src/main/resources/static/assets/js/index.js
@@ -7,7 +7,7 @@ const handleAddQuery = (key, value) => {
 
 // Tạo URL mới với các tham số truy vấn mới được thêm vào
     const newUrl = window.location.pathname + '?' + searchParams.toString();
-
+    
 // Chuyển hướng đến URL mới
     window.location.href = newUrl;
 }
@@ -20,3 +20,42 @@ categories.forEach(item => {
         handleAddQuery("category",id);
     }
 })
+
+const formSearch = document.getElementById('form-search');
+
+formSearch.onsubmit = (e) => {
+    handleSearch(e);
+}
+
+const handleSearch =  (e) => {
+    e.preventDefault();
+    const inputValue = document.getElementById("search-input").value;
+    if(inputValue.trim().length > 0){
+        handleAddQuery("name",inputValue);
+    }
+}
+const valueSearch= ()=> {
+    const searchParams = new URLSearchParams(window.location.search);
+    const param1Value = searchParams.get('name');
+    return param1Value;
+}
+document.getElementById("search-input").value= valueSearch();
+const activeBestSeller= ()=> {
+    const searchParams = new URLSearchParams(window.location.search);
+    const param1Value = searchParams.get('sort');
+    if(param1Value === "best-seller"){
+        document.getElementById("btn-bestSeller").style.backgroundColor = "rgb(32,135,67)";
+    }
+}
+const activeCategory= ()=> {
+    const searchParams = new URLSearchParams(window.location.search);
+    const param1Value = searchParams.get('category');
+    categories.forEach(item => {
+        const id = item.getAttribute('data-id');
+        if(param1Value === id){
+            document.querySelector(`[data-id="${id}"]`).style.textDecoration = 'underline';
+        }
+    })
+}
+activeCategory();
+activeBestSeller();
